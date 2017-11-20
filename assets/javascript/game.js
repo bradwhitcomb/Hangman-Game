45de1dc4f27c664bb360aaff7c3cf1911bc347e2
JBW <whitco670@jbw-mbp-215.attlocal.net>
2017-11-20 18:41:57 -0500

    runout of time and energy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,25 @@
-	console.log("is anybody home?");
+	
 	
 
 
 	$(document).ready(function() {
 		var wins = 0;
-
 		var guessRemaining = 10;
-		
-		
 		var splitWord = 0;
 		var winner = [];
+
 		$("#wordGenerator").on("click", function() {
-			
+			//document.onkeyup = function(event){
 			//List of words for the game
 			var wordList=["hiker","trail","mountain","valley","peak","shelter","bear"];
 
 			//Randomly choose word from and split it
 			var wordGenerator = 0;
 			winner = [];
-			console.log("The winner list is "+ winner);
 			wordGenerator=Math.floor(Math.random() * (wordList.length));
 			var selectedWord=wordList[wordGenerator];
 			splitWord=selectedWord.split("");
-			console.log("Splitword is " + splitWord);
-			//console.log(splitWord[1]);
+		
 
 			//create missing letter table for the game 
 			for(var letters = 0; letters<splitWord.length; letters++){
@@ -34,39 +30,30 @@
 				letterBtn.text(splitWord[letters]);
 				$(".wordBox").append(letterBtn);
 
-				//var dash = "____ "
-				//$("#hiddenLetters").append(dash);
-				//console.log(letters);
-
-				//console.log(splitWord);
-		}
+		}		
+		
 			
 		
-
-     })
-			console.log("checking in");
+			})
+     
+		
 			document.onkeyup = function(event){
 			var userGuess = event.key;
-			alert(userGuess);
 			$("#letterGuess").append(userGuess);
 			guessRemaining = guessRemaining - 1;
-			console.log("user Guess is " + userGuess + "guessRemaining is "+guessRemaining);
 			$("#scoreboard1").text(guessRemaining);
 
 
 			for(var letters = 0; letters<splitWord.length; letters ++){
 				var gameLetter = splitWord[letters];
-				console.log("game letter is " + gameLetter);
-					if (userGuess === gameLetter){
-						console.log("it's a match");
-						winner.push(userGuess);
-						console.log(winner);
 
-					
+			
+					if (userGuess === gameLetter){					
+						winner.push(userGuess);
+							
 						
 						if (winner.length === splitWord.length){
 						wins = wins + 1;
-						console.log("WINS " + wins);
 						$("#scoreboard").text(wins);
 
 				}
